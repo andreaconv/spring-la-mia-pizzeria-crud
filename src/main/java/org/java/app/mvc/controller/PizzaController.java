837ae668c096db3f044fd95d0cd96b01684601eb
andreaconv <andrea2convertini@gmail.com>
2023-10-04 18:30:24 +0200

    [add] bootstrap + header e pagina dettaglio

--- src/main/java/org/java/app/mvc/controller/PizzaController.java
+++ src/main/java/org/java/app/mvc/controller/PizzaController.java
@@ -8,21 +8,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pizze")
+@RequestMapping()
 public class PizzaController {
 
 	@Autowired
 	private PizzaService pizzaService;
 	
-	@GetMapping
+	@GetMapping("/")
+	public String index(Model model) {
+		
+//		final String name = "Andrea";
+//		
+//		model.addAttribute("name", name);
+		
+		return "index";
+	}
+	
+	@GetMapping("/pizze")
 	public String getPizze(Model model) {
 		
 		List<Pizza> pizze = pizzaService.findAll();
 		model.addAttribute("pizze", pizze);
 		return "pizze-index";
+		
+	}
+	
+	@GetMapping("/pizze/{id}")
+	public String getShow(@PathVariable int id, Model model) {
+		
+		Pizza pizza = pizzaService.findById(id);
+		model.addAttribute("pizza", pizza);
+		return "pizza-show";
 		
 	}
 }
