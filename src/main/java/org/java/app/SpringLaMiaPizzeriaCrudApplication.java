7bbef7a60391c2c55e3273cc76266d3632dd64c6
andreaconv <andrea2convertini@gmail.com>
2023-10-05 16:45:00 +0200

    [edit] pizza-show

--- src/main/java/org/java/app/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/org/java/app/SpringLaMiaPizzeriaCrudApplication.java
@@ -1,5 +1,8 @@
 package org.java.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.java.app.db.pojo.Pizza;
 import org.java.app.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +22,32 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
-//		String nome, String descrizione, String foto, float prezzo
-		Pizza margherita = new Pizza("margherita", "pomodoro, mozzarella, basilico", "url-foto-margherita", 6.50f);
-		Pizza diavola = new Pizza("diavola", "pomodoro, mozzarella, salame piccante", "url-foto-diavola", 8.50f);
-		Pizza ortolana = new Pizza("ortolana", "mozzarella, peperoni, melanzane, zucchine", "url-foto-ortolana", 7.50f);
-		
-		pizzaService.save(margherita);
-		pizzaService.save(diavola);
-		pizzaService.save(ortolana);
-		
-		System.out.println("Insert OK!");
+	    List<Pizza> pizze = Arrays.asList(
+	            new Pizza("marinara", "pomodoro, origano, aglio", "marinara", 4.5f),
+	            new Pizza("margherita", "pomodoro, mozzarella, basilico", "margherita", 6.5f),
+	            new Pizza("wurstel", "pomodoro, mozzarella, wurstel", "wurstel", 5.5f),
+	            new Pizza("diavola", "pomodoro, mozzarella, salame piccante", "diavola", 8.5f),
+	            new Pizza("prosciutto", "pomodoro, mozzarella, prosciutto cotto", "prosciutto", 6f),
+	            new Pizza("napoletana", "pomodoro, mozzarella, acciughe, origano", "napoletana", 5.5f),
+	            new Pizza("pugliese", "pomodoro, mozzarella, cipolla", "pugliese", 5.5f),
+	            new Pizza("romana", "pomodoro, mozzarella, acciughe, capperi, origano", "romana", 6.5f),
+	            new Pizza("prosciutto e funghi", "pomodoro, mozzarella, prosciutto cotto, funghi", "prosc-funghi", 6.5f),
+	            new Pizza("quattro stagioni", "pomodoro, mozzarella, prosciutto cotto, funghi, carciofi, olive", "stagioni", 8.5f),
+	            new Pizza("parmigiana", "pomodoro, mozzarella, melanzane, grana", "parmigiana", 6f),
+	            new Pizza("capricciosa", "pomodoro, mozzarella, carciofi, funghi, olive, capperi, origano, acciughe", "capricciosa", 8f),
+	            new Pizza("quattro formaggi", "mozzarella, zola, scamorza, stracciatella di bufala", "formaggi", 8f),
+	            new Pizza("speck e zola", "pomodoro, mozzarella, speck, zola", "speck", 7f),
+	            new Pizza("estate", "pomodoro, mozzarella, pomodoro fresco, basilico", "estate", 6f),
+	            new Pizza("zucchine", "pomodoro, mozzarella, zucchine, grana", "zucchine", 6f),
+	            new Pizza("bufalina", "pomodoro, mozzarella bufala campana, basilico", "bufalina", 7f),
+	            new Pizza("prosciutto e carciofi", "pomodoro, mozzarella, prosciutto cotto, carciofi", "prosc-carciofi", 6.5f),
+	            new Pizza("tonno e cipolla", "pomodoro, mozzarella, tonno, cipolla", "tonno", 6.5f),
+	            new Pizza("ortolana", "mozzarella, peperoni, melanzane, zucchine, funghi", "ortolana", 7.5f)
+	    );
+	    
+	    pizze.forEach(pizzaService::save);
+	    
+	    System.out.println("Insert OK!");
 	}
 	
 
